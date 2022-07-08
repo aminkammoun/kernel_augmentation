a3cdc2dc0c5a25b903d670f3301166d5d0393151
aminkammoun <aminekammoun55@gmail.com>
2022-07-08 15:42:11 +0100

    1st commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app dark>
+    <Navbar />
+    <v-main>
+      <v-container fluid> <Nuxt /></v-container>
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navbar from '~/components/navbar/index.vue'
+import Footer from '~/components/footer/index.vue'
+export default {
+  data() {
+    return {
+      fixed: false,
+    }
+  },
+  components: { Navbar, Footer },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <Main />
+</template>
+<script>
+export default {}
+</script>
