890141d2bcc36977c5be210436afe23f520856f9
aminkammoun <aminekammoun55@gmail.com>
2022-07-08 15:42:24 +0100

    2nd commit

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  ssr: false,
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - kernel_augmentation',
+    title: 'kernel_augmentation',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: ['~/components'],
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    optionsPath: './vuetify.options.js',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ vuetify.options.js
@@ -0,0 +1,25 @@
+export default {
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#33c',
+        accent: '#33c',
+        secondary: '#33c',
+        info: '#33c',
+        warning: '#33c',
+        error: '#33c',
+        success: '#33c',
+      },
+      light: {
+        primary: '#4C3A51',
+        accent: '#4C3A51',
+        secondary: '#4C3A51',
+        info: '#4C3A51',
+        warning: '#4C3A51',
+        error: '#4C3A51',
+        success: '#4C3A51',
+      },
+    },
+  },
+}
