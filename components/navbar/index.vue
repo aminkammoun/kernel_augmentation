7e415835165febc0824314a46a1c429e6d858e94
aminkammoun <aminekammoun55@gmail.com>
2022-07-09 00:55:47 +0100

    night$

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -1,35 +1,19 @@
 <template>
   <div>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app dense>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-btn>
+        <span>about us</span>
+      </v-btn>
+      <v-btn @click.stop="switchTheme()">
+        <span>contact us</span>
+      </v-btn>
       <v-btn icon @click.stop="switchTheme()">
-        <v-icon>mdi-menu</v-icon>
+        <v-icon v-if="!this.$vuetify.theme.isDark"
+          >mdi-moon-waning-crescent</v-icon
+        >
+        <v-icon v-else>mdi-weather-sunny</v-icon>
       </v-btn>
     </v-app-bar>
   </div>
