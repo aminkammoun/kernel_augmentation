d7328bbb9670aac0911a5c936706892f51940659
aminkammoun <aminekammoun55@gmail.com>
2022-07-20 23:30:23 +0100

    contact us

--- components/contactus/index.vue
+++ components/contactus/index.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <v-card>
+    <v-form>
+      <v-row>
+        <v-col cols="12" md="6"
+          ><v-text-field label="email" v-model="email" outlined></v-text-field
+        ></v-col>
+        <v-col cols="12" md="6"
+          ><v-text-field label="name" v-model="name" outlined></v-text-field
+        ></v-col>
+        <v-col cols="12" md="12"
+          ><v-textarea v-model="description" outlined></v-textarea
+        ></v-col> </v-row></v-form
+  ></v-card>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    email: '',
+    name: '',
+    description: '',
+  }),
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
